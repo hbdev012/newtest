07f0a94de45565b9cd9affb9b7b0fef0ac1e4ddd
Natwar <hbdev013@phptalent.com>
2012-07-21 15:40:26 +0530

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoHaml::Application.config.secret_token = '3bde5bfc86cc1db429c37dd05512203138817125b4c94ffc60af57e1ffe9945a8b16f25ad32f3d5719afb90bbea9cffcff7390630b62e8ce095facddf82ab63b'
